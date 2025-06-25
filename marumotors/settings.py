901cb2aba263f61db655a133fbd8d3dc152ff2ed
Stefany Noguera <stefanynoguerar@gmail.com>
2025-06-25 17:52:43 +0200

    removed cloudinary debug

--- marumotors/settings.py
+++ marumotors/settings.py
@@ -15,12 +15,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-print("Cloudinary config:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-print("DISABLE_CLOUDINARY:", os.getenv("DISABLE_CLOUDINARY"))
-
-
-print("Cloudinary config:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
