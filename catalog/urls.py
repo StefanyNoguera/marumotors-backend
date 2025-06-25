1a29622ee2d66a0354f59d7051c841ace11f82df
Stefany Noguera <stefanynoguerar@gmail.com>
2025-06-25 18:00:46 +0200

    running migrations

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,11 +3,12 @@ from .api_views import (
     CarListView, CarDetailView,
     AutopartListView, AutopartDetailView
 )
+from .views import run_setup
 
 urlpatterns = [
     path('cars/', CarListView.as_view(), name='car-list'),
     path('cars/<int:id>/', CarDetailView.as_view(), name='car-detail'),
     path('autoparts/', AutopartListView.as_view(), name='autopart-list'),
     path('autoparts/<int:id>/', AutopartDetailView.as_view(), name='autopart-detail'),
-    # path('setup/', run_setup),
+    path('setup/', run_setup),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,13 @@
-# from django.http import HttpResponse
-# from django.core.management import call_command
-# from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
-# def run_setup(request):
-#     call_command('migrate')
+def run_setup(request):
+    call_command('migrate')
 
-#     User = get_user_model()
-#     if not User.objects.filter(username='admin').exists():
-#         User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')
 
-#     return HttpResponse("Migrations applied and superuser created.")
+    return HttpResponse("Migrations applied and superuser created.")
     
